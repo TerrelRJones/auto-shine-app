4e2ba42f046b6d7c09b79392c8a64503409d99fb
Terrel <TerrelRJones@gmail.com>
2022-02-03 00:58:45 -0800

    Fixed commit issues, all is well. I hope.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 // import { app } from "./app";
 const app = require("./app");
 
-// const port: number | string = process.env.PORT || 4001;
+const port: number | string = process.env.PORT || 4001;
 
-app.listen(process.env.PORT || 4001, () => {
-  console.log(`Booming on port:${process.env.PORT}. It's LIT!`);
+app.listen(port, () => {
+  console.log(`Booming on port:${port}. It's LIT!`);
 });
