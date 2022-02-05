6264d96ccfb4265630ff841f2ecfb0297ddf32a7
Terrel <TerrelRJones@gmail.com>
2022-02-05 14:16:52 -0800

    added price to service data

--- client/app/data/service.js
+++ client/app/data/service.js
@@ -3,21 +3,25 @@ const data = [
     id: "1",
     product: "Standard Wash",
     img: require("../assets/img/wash.png"),
+    price: 30,
   },
   {
     id: "2",
     product: "Interior",
     img: require("../assets/img/interiorSeat.png"),
+    price: 50,
   },
   {
     id: "3",
     product: "Polish",
     img: require("../assets/img/polishHands.png"),
+    price: 80,
   },
   {
     id: "4",
     product: "Premium Detail",
     img: require("../assets/img/carCleanPerson.png"),
+    price: 220,
   },
 ];
 
