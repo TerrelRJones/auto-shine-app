b7a2f18d0b62af2a7ab4862d86e7d74784e0d7c0
Terrel <TerrelRJones@gmail.com>
2022-02-05 14:20:11 -0800

    fixed req.body params

--- src/routes/address.routes.ts
+++ src/routes/address.routes.ts
@@ -5,24 +5,25 @@ const router: Router = Router();
 import prisma from "../client";
 
 router.post("/createAddress", async (req: Request<Address>, res: Response) => {
-  const { state, city, street, addressId } = req.body;
-  const zip = parseInt(req.body);
+  const { state, city, street, zip, addressId } = req.body;
 
   const address = await prisma.address.create({
     data: {
-      state,
-      city,
-      zip,
-      street,
-      addressId,
+      state: state,
+      city: city,
+      zip: parseInt(zip),
+      street: street,
+      addressId: addressId,
     },
   });
 
   return res.status(200).json(address);
 });
 
+router.get("/getAddress", async (req: Request, res: Response) => {});
+
 router.delete("/deleteAddress/:id", async (req, res) => {
-  const deletedAddress = prisma.address.delete({
+  const deletedAddress = await prisma.address.delete({
     where: { id: req.params.id },
   });
   return res.status(200).json(deletedAddress);
