8ef8e1594cc9a5c22b3582bf335e816c50bf4d39
Terrel <TerrelRJones@gmail.com>
2022-02-06 11:26:35 -0800

    added stripe provider

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AuthProvider } from "./app/contexts/Auth";
 import { Router } from "./app/routes/Router";
+// import {StripeProvider} from "@stripe/stripe-react-native"
 
 export default function App() {
   return (
     <AuthProvider>
+      {/* <StripeProvider publishableKey="" > */}
       <Router />
+      {/* </StripeProvider> */}
     </AuthProvider>
   );
 }
