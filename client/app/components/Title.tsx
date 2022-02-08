0abce9158e47784e93e20d0b3d9f999c4e040d72
Terrel <TerrelRJones@gmail.com>
2022-02-08 01:52:32 -0800

    changed title style

--- client/app/components/Title.tsx
+++ client/app/components/Title.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import { color } from "./colors";
 
 interface TitleProp {
   title: string;
@@ -12,8 +13,9 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 25,
+    fontSize: 30,
     fontWeight: "800",
     textTransform: "capitalize",
+    color: `${color.primary}`,
   },
 });
