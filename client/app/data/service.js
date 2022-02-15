2f4da08a518f67d49593b8d92a87d674e70fe9d7
Terrel <TerrelRJones@gmail.com>
2022-02-15 13:17:21 -0800

    updated icons

--- client/app/data/service.js
+++ client/app/data/service.js
@@ -2,25 +2,25 @@ const data = [
   {
     id: "1",
     product: "Standard Wash",
-    img: require("../assets/img/wash.png"),
+    img: require("../assets/img/carWash.png"),
     price: 30,
   },
   {
     id: "2",
     product: "Interior",
-    img: require("../assets/img/interiorSeat.png"),
+    img: require("../assets/img/interiorSeat2.png"),
     price: 50,
   },
   {
     id: "3",
     product: "Polish",
-    img: require("../assets/img/polishHands.png"),
+    img: require("../assets/img/polishHands2.png"),
     price: 80,
   },
   {
     id: "4",
     product: "Premium Detail",
-    img: require("../assets/img/carCleanPerson.png"),
+    img: require("../assets/img/carCleanPerson2.png"),
     price: 220,
   },
 ];
